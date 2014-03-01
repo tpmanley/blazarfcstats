da3cc912a31ccdb32cf7e69f46d4e42e4d469507
Tom Manley <tom.manley@gmail.com>
2014-03-01 16:00:37 -0600

    Added Facebook login with authomatic

--- app.py
+++ app.py
@@ -2,10 +2,26 @@ import os
 from flask import Flask, request, make_response, render_template
 from flask.ext.restless import APIManager
 from flask.ext.sqlalchemy import SQLAlchemy
+from authomatic.providers import oauth2
+from authomatic.adapters import WerkzeugAdapter
+from authomatic import Authomatic
+
+CONFIG = {
+    'fb': {
+        'class_': oauth2.Facebook,
+        # Facebook is an AuthorizationProvider too.
+        'consumer_key': os.environ.get('FACEBOOK_KEY'),
+        'consumer_secret': os.environ.get('FACEBOOK_SECRET'),
+        # But it is also an OAuth 2.0 provider and it needs scope.
+        'scope': ['user_about_me', 'email', 'publish_stream'],
+    },
+}
 
 app = Flask(__name__, static_url_path="")
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///stats.db')
 db = SQLAlchemy(app)
+
+authomatic = Authomatic(CONFIG, 'your secret string', report_errors=False)
 
 
 class Player(db.Model):
@@ -21,6 +37,39 @@ api_manager.create_api(Player, methods=['GET', 'PUT'])
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/login/<provider_name>/', methods=['GET', 'POST'])
+def login(provider_name):
+    """
+    Login handler, must accept both GET and POST to be able to use OpenID.
+    """
+
+    # We need response object for the WerkzeugAdapter.
+    response = make_response()
+
+    # Log the user in, pass it the adapter and the provider name.
+    result = authomatic.login(WerkzeugAdapter(request, response), provider_name)
+
+    # If there is no LoginResult object, the login procedure is still pending.
+    if result:
+        if result.user:
+            # We need to update the user to get more info.
+            result.user.update()
+
+            fb_pic_url = "https://graph.facebook.com/%s/picture?height=200&type=normal&width=200" % result.user.id
+            response = result.provider.access(fb_pic_url, content_parser=lambda body:body)
+            if response.status == 200:
+                pic_name = result.user.username + ".jpg"
+                with open(os.path.join(os.path.dirname(__file__), 'static', 'images', pic_name), "wb") as f:
+                    f.write(response.data)
+
+        # The rest happens inside the template.
+        return render_template('index.html', result=result)
+
+    # Don't forget to return the response.
+    return response
+
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
