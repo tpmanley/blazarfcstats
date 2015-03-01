377523a260f05d1944edc9359eb45362cfd70fe0
Tom Manley <tom.manley@gmail.com>
2015-02-28 21:00:09 -0600

    Fetch FB profile pics right from FB

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from authomatic.providers import oauth2
 from authomatic.adapters import WerkzeugAdapter
 from authomatic import Authomatic
-import requests
 
 CONFIG = {
     'fb': {
@@ -60,14 +59,8 @@ def login(provider_name):
             player = Player.query.filter_by(name=result.user.name).first()
             if player:
                 fb_pic_url = "https://graph.facebook.com/%s/picture?height=200&type=normal&width=200" % result.user.id
-                response = requests.get(fb_pic_url)
-                if response.status_code == 200:
-                    pic_name = result.user.username + ".jpg"
-                    with open(os.path.join(os.path.dirname(__file__), 'static', 'images', pic_name), "wb") as f:
-                        for chunk in response.iter_content():
-                            f.write(chunk)
-                    player.picture = "/images/" + pic_name
-                    db.session.commit()
+                player.picture = fb_pic_url
+                db.session.commit()
 
         # The rest happens inside the template.
         return redirect('/')
